378acb10e8c7c6eb3fa421c610c42cddf087b1c2
mlateb <latebmelissa@gmail.com>
2024-10-24 15:52:40 +0200

    [FIX] - Correction du bug de boutton

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -25,15 +25,14 @@ const Card: React.FC<CardProps> = ({ card, onClickSell, onClickBuy, user}) => {
       <div className="card-details">
         <h3>{card.name}</h3>
         <p>Rarity: <span className={`rarity ${card.rarity.toLowerCase()}`}>{card.rarity}</span></p>
-        {card.onSell ? (
-          <button className="redeem-button disabled">Already on sale</button>
-        ) : (
+        {card.owner === user && !card.onSell ? (
           <button onClick={onClickSell} className="redeem-button">Sell</button>
-        )}
-        {card.owner != user ? (
-          <button onClick={onClickBuy} className="redeem-button">Buy</button>
         ) : (
-          <p></p>
+          card.owner === user && card.onSell ? (
+            <button className="redeem-button disabled">Already on sale</button>
+          ) : (
+            <button onClick={onClickBuy} className="redeem-button">Buy</button>
+          )
         )}
       </div>
     </div>
